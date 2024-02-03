108f6e42d33eabc1d720c6af50b44dbf9a388174
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-02-03 22:03:28 +0100

    Initial Commit

---
+++ DAY25_RockPaperScissors/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DAY25_RockPaperScissors
+//
+//  Created by Vladyslav Dikhtiaruk on 03/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DAY25_RockPaperScissors/DAY25_RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DAY25_RockPaperScissorsApp.swift
+//  DAY25_RockPaperScissors
+//
+//  Created by Vladyslav Dikhtiaruk on 03/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct DAY25_RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
